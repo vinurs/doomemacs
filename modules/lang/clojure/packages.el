4403084da018784a9a4a8f1c0c16ace670f07775
vinurs <haiyuan.vinurs@gmail.com>
2022-10-25 10:54:56 +0800

    update clj-refactor

--- modules/lang/clojure/packages.el
+++ modules/lang/clojure/packages.el
@@ -15,7 +15,7 @@
 
 ;;; Core packages
 (package! clojure-mode :pin "414157c3e523e80cc44dca8f86f1853122ee5f6b")
-(package! clj-refactor :pin "db89b558710d31d4b9dfe7c4d2a07df9b498d4a6")
+(package! clj-refactor :pin "8300d5cab861668f313fbbbb3e2926e3e5130e86")
 (package! cider :pin "92c1b6de20805a9caa556cc671ac39080e4e89cc")
 (when (modulep! :checkers syntax)
   (package! flycheck-clj-kondo :pin "ff7bed2315755cfe02ef471edf522e27b78cd5ca"))
